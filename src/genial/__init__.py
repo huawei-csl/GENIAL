34ad19807215515b154d958cc8051c2333255b57
Ryan Amaudruz <r84389078@china.huawei.com>
2025-12-10 16:02:51 +0100

    edited flowy ref to FLOWY

--- src/genial/__init__.py
+++ src/genial/__init__.py
@@ -5,7 +5,7 @@ import importlib.util
 load_dotenv()
 
 # Detect optional dependency availability without importing it
-if importlib.util.find_spec("flowy") is not None:
+if importlib.util.find_spec("FLOWY") is not None:
     global_vars["flowy_available"] = True
 else:
     global_vars["flowy_available"] = False
