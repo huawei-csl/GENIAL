adff0d074e1f21c0920ba900caf5d549a1505394
Ryan Amaudruz <r84389078@china.huawei.com>
2025-12-19 09:37:12 +0100

    res2

--- src/genial/loop/full_run_v2.py
+++ src/genial/loop/full_run_v2.py
@@ -370,7 +370,7 @@ class SlurmDispatcher:
 
     @staticmethod
     def submit_script(script_path, extra_args: list[str] = None, is_dry_run=False):
-        cmd = ["sbatch"]
+        cmd = ["sbatch"] + ["--reservation=ai-team"]
         if extra_args:
             cmd += extra_args
         cmd.append(script_path)
