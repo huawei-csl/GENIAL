9b7461d3426eb183b80c737d4e26de8d0733205a
Ryan Amaudruz <r84389078@china.huawei.com>
2026-02-26 17:01:31 +0100

    pp

--- src/genial/ext_plugins/flowy/flowy_launcher_helper.py
+++ src/genial/ext_plugins/flowy/flowy_launcher_helper.py
@@ -285,7 +285,9 @@ class FlowyLauncherHelper:
                 if full_dir_path.exists():
                     shutil.copy(full_dir_path, self.design_output_dir_path / new_name)
                 else:
-                    raise ValueError(f"Case not handled!")
+                    print(f'Error occurred')
+                    print(f'full_dir_path: {full_dir_path}')
+                    print(f'self.design_output_dir_path / new_name: {self.design_output_dir_path / new_name}')
 
         def collect_output_txts(temp_dir: str | Path, out_file: str | Path) -> None:
             """
