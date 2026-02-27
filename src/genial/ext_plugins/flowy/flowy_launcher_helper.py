f1a7a891dcf20cafd22d1f646ffc7caf11855be2
Ryan Amaudruz <r84389078@china.huawei.com>
2026-02-27 09:25:01 +0100

    2 runs

--- src/genial/ext_plugins/flowy/flowy_launcher_helper.py
+++ src/genial/ext_plugins/flowy/flowy_launcher_helper.py
@@ -218,7 +218,7 @@ class FlowyLauncherHelper:
                     return_record=True,
                     multiproc=not global_vars["debug"],
                 )
-                print(all_run_ids)
+                logger.warning(f'all_run_ids: {all_run_ids}')
         except KeyError:
             logger.warning(f"There was an error when analyzing the results of the flowy runs.")
             logger.info(traceback.format_exc())
